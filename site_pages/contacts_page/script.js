1b635bc9b25fba4f7c3b97b7467420e7d0c46a34
Michael <Maximvo92@gmail.com>
2021-05-11 16:22:03 +0300

    contacts page in process

--- site_pages/contacts_page/script.js
+++ site_pages/contacts_page/script.js
@@ -1,39 +1,12 @@
-                      // Слайдер
-
-/* Устанавливаем индекс слайда по умолчанию */
-let slideIndex = 1;
-showSlides(slideIndex);
-
-/* Увеличиваем индекс на 1 — показываем следующий слайд*/
-function nextSlide() {
-    showSlides(slideIndex += 1);
-}
-
-/* Уменьшает индекс на 1 — показываем предыдущий слайд*/
-function previousSlide() {
-    showSlides(slideIndex -= 1);
-}
-
-/* Устанавливаем текущий слайд */
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-/* Функция перелистывания */
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("item");
-
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-
-  /* Проходим по каждому слайду в цикле for */
-    for (let slide of slides) {
-        slide.style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
-}
+document.getElementById('file').addEventListener('change', function (e) {
+  // Проверяем, что только 1 файл был выбран
+  if (this.files && this.files.length == 1) {
+      const textContainer = this.nextElementSibling.querySelector('.contacts__form-fileButtonText');
+      const fileName = e.target.value.split('\\').pop();
+      if (textContainer) {
+          textContainer.textContent = fileName || 'Выберите файл для загрузки';
+          return true;
+      }
+  }
+  return false;
+});

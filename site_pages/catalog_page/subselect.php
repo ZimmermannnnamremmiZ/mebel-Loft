773f23ed103d22193c1e21cb1685374c1e95380c
FIRST_NAME LAST_NAME <Maximvo92@gmail.com>
2021-06-29 12:06:56 +0300

    added dependent select_1 and select_2

--- site_pages/catalog_page/subselect.php
+++ site_pages/catalog_page/subselect.php
@@ -1,4 +1,4 @@
-<script>
+<script type="text/javascript">
   $(function() {
     $('select[name="subselect"]').change(function() {
       alert($('select[name="subselect"]').val());
@@ -6,22 +6,22 @@
   });
 </script>
 <?php
- $dbUser = 'root';
-    $dbName = 'selects';
-    $dbPass = '';
-    $mysqli = new mysqli('localhost', $dbUser, $dbPass, $dbName);
-    $query = 'set names utf8';
-    $mysqli->query($query);
-    $query = 'select * from `select_2` where `id_select_1` = $id';
-    $results = $mysqli->query($query);
+  $dbUser = 'root';
+  $dbName = 'selects';
+  $dbPass = '';
+  $mysqli = new mysqli('localhost', $dbUser, $dbPass, $dbName);
+  $query = 'set names utf8';
+  $mysqli->query($query);
+  $id = intval($_POST['id']);
+  $query = "select * from `select_2` where `id_select_1` = $id";
+  $results = $mysqli->query($query);
   if (isset($_POST['id']) && !empty($_POST['id'])) {
-    $id = intval($_POST['id']);
-    echo "<select name='subselect'>";
-    while($row = $results->fetch()) {
+    echo "<select class='catalog__select' name='subselect'>";
+    while($row = $results->fetch_assoc()) {
       echo "<option>{$row["title"]}</option>";
     }
-    echo "</select>";
+      echo "</select>";
   } else {
-    echo "<select name='subselect' disabled><option value='0'>--Выберете подкатегорию--</option></select>";
+      echo "<select class='catalog__select' name='subselect' disabled><option value='0'>Сперва выберете раздел</option></select>";
   }
 ?>

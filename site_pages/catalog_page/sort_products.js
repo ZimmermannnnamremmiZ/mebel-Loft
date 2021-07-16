0f69fa52d11bed2e7c6907b194c935ca3cfe0762
FIRST_NAME LAST_NAME <Maximvo92@gmail.com>
2021-07-16 16:43:10 +0300

    '

--- site_pages/catalog_page/sort_products.js
+++ site_pages/catalog_page/sort_products.js
@@ -1,29 +1,43 @@
 // sorting products by price
 
 const sortToMaxBtn = document.getElementById("toMaxPrice");
-
+const elements = document.querySelectorAll('.bestsellers__box-item');
+const resultEl = document.querySelector(".bestsellers__box");
 sortToMaxBtn.addEventListener("click", () => {
-  const elements = document.querySelectorAll('.bestsellers__box-item');
   const sorted = [...elements].sort((a, b) => {
     const priceElA = a.querySelector(".bestsellers__item-price");
     const priceElB = b.querySelector(".bestsellers__item-price");
     const getPrice = (el) => parseInt(el.innerHTML.replace(/ /g, ""));
     return getPrice(priceElA) - getPrice(priceElB);
   });
-  const resultEl = document.querySelector(".bestsellers__box");
+
   sorted.forEach(el => resultEl.appendChild(el));
 });
 
 const sortToMinBtn = document.getElementById("toMinPrice");
 
 sortToMinBtn.addEventListener("click", () => {
-  const elements = document.querySelectorAll('.bestsellers__box-item');
+
   const sorted = [...elements].sort((a, b) => {
     const priceElA = a.querySelector(".bestsellers__item-price");
     const priceElB = b.querySelector(".bestsellers__item-price");
     const getPrice = (el) => parseInt(el.innerHTML.replace(/ /g, ""));
     return getPrice(priceElB) - getPrice(priceElA);
   });
-  const resultEl = document.querySelector(".bestsellers__box");
+
   sorted.forEach(el => resultEl.appendChild(el));
+  fnm()
 });
+
+
+
+function fnm() {
+  [...elements].forEach(el => {if (el.querySelector(".bestsellers__item-name").innerText.includes($(".brandItem:checked").map(function() {
+      return $(this).val();         
+      }).get())) {
+        el.style.display = "block";
+  } else {
+    el.style.display = "none";
+  }
+})}
+

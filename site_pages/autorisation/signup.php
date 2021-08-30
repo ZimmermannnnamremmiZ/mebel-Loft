f163ec9a6a9a3b372099e53cbb59b2afd7a9f587
ZimmermannnnamremmiZ <Maximvo92@gmail.com>
2021-08-31 00:33:24 +0300

    1024px fix

--- site_pages/autorisation/signup.php
+++ site_pages/autorisation/signup.php
@@ -13,10 +13,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="../scripts/owl carousel/owl.carousel.min.css">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="../scripts/owl carousel/owl.carousel.min.js"></script>
-    <link rel="stylesheet" href="style.css">
   </head>
     <body>
       <div class="container">

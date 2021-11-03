e17b621505ffac643e90f885f97179c2dafcd6d8
FIRST_NAME LAST_NAME <Maximvo92@gmail.com>
2021-11-03 16:55:21 +0300

    257 bascet_page

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Мебель Loft</title>
+  <link rel="icon" href="images/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="/site_pages/title_page/style.css">
   <link rel="stylesheet" href="/site_pages/scripts/owl carousel/owl.carousel.min.css">
   <link rel="stylesheet" href="/reusable_css/hamburger_menu.css">

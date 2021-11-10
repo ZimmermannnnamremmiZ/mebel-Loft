1088a11fd8efad8613ae78626bd8f2bc6d73eaf5
ZimmermannnnamremmiZ <Maximvo92@gmail.com>
2021-11-11 01:54:53 +0300

    added some options on click on .catalog__filterBtn

--- site_pages/catalog_page/filterBtn_for_1024px.js
+++ site_pages/catalog_page/filterBtn_for_1024px.js
@@ -1,8 +1,7 @@
 $(".catalog__filterBtn").click(function() {
+    $(".catalog__menu-Container").css("display", "block");
     $(".catalog__menu").css("display", "flex");
-    $(".catalog__filterBtn").css("display", "none")
-    $(".bestsellers__box").css({
-        "padding-left": "10px",
-        "margin-top": "0"
-    })                   
+    $(".catalog__filterBtn").css("display", "none");
+    $(".bestsellers__box").css("width", "70%");
+    $(".bestsellers__box-item").css("width", "32%")
 })

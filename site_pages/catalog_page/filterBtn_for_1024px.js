005e0ca9f6ddb37b79b1822ba4c43270dc873455
ZimmermannnnamremmiZ <Maximvo92@gmail.com>
2021-11-15 00:29:54 +0300

    added adaptive for catalog__page

--- site_pages/catalog_page/filterBtn_for_1024px.js
+++ site_pages/catalog_page/filterBtn_for_1024px.js
@@ -1,7 +1,66 @@
-$(".catalog__filterBtn").click(function() {
+$(".catalog__filterBtn").click(function () {
     $(".catalog__menu-Container").css("display", "block");
     $(".catalog__menu").css("display", "flex");
-    $(".catalog__filterBtn").css("display", "none");
-    $(".bestsellers__box").css("width", "70%");
-    $(".bestsellers__box-item").css("width", "32%")
+    $(".hamburger-menu-2").css("margin-top", "10px")
+    if ($(window).width() > 980) {
+        $(".catalog__filterBtn-x").css("display", "none");
+        $(".catalog__filterBtn").css("display", "none");
+        $(".bestsellers__box").css({
+            "grid-template-columns": "repeat(3, 1fr)",
+            "margin-top": "30px",
+            "margin-left": "30px"
+        });
+    } else if ($(window).width() > 740) {
+        $(".catalog__filterBtn-x").css("display", "none");
+        $(".catalog__filterBtn").css("display", "none");
+        $(".bestsellers__box").css({
+            "grid-template-columns": "repeat(2, 1fr)",
+            "margin-top": "30px",
+            "margin-left": "30px ",
+            "width": "61%"
+        });
+        $(".catalog__menu-Container").css({
+            "width": "30%"
+        })
+    } else if ($(window).width() > 560) {
+        $(".catalog__filterBtn-x").css("display", "none");
+        $(".catalog__filterBtn").css("display", "none");
+        $(".bestsellers__box").css({
+            "grid-template-columns": "repeat(2, 1fr)",
+            "margin-top": "30px",
+            "margin-left": "5px ",
+            "width": "61%",
+            "grid-gap": "5px"
+        });
+        $(".catalog__menu-Container").css({
+            "width": "30%"
+        })
+    } else {
+        $(".catalog__filterBtn-x").css({
+            "display": "block",
+            "padding": "0 10px"
+        });
+        $(".catalog__menu-Container").css({
+            "display": "block",
+            "position": "absolute",
+            "width": "440px",
+            "background-color": "#fff",
+            "z-index": "2",
+        });
+        $(".catalog__filterBtnBox").css({
+            "position": "absolute",
+            "background-color": "inherit",
+            "margin-top": "30px",
+            "z-index": "3",
+            "width": "480px"
+        });
+        $(".catalog__filterBtn").css({
+            "position": "static",
+            "margin-top": "0",
+            "width": "100%",
+            "background-color": "inherit",
+            "font-size": "14px"
+        });
+        $(".catalog__menu").css("margin-top", "40px")
+    }
 })
